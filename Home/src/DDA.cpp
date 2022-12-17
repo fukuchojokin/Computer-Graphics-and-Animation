4354d6ab8b5206b494018e73f22cfad0fbdd583c
bestware <kshitiz.wagle@yahoo.com>
2022-12-17 19:58:11 +0545

    readme

--- Home/src/DDA.cpp
+++ Home/src/DDA.cpp
@@ -3,14 +3,16 @@
 #include <math.h>
 #include <graphics.h>
 #include <stdlib.h>
-int main() {
+int main()
+{
     int gdriver = DETECT, gmode;
     int x1, x2, y1, y2, i, step, xn, yn, dx, dy;
-    // char data[] = "C:\\MinGW\\lib\\libbgi.a";
     printf("Enter x1,y1: ");
     scanf("%d,%d", &x1, &y1);
     printf("Enter x2,y2: ");
     scanf("%d,%d", &x2, &y2);
+    // char data[] = "C:\\MinGW\\lib\\libbgi.a";
+    // initgraph(&gdriver, &gmode, data);
     initgraph(&gdriver, &gmode, NULL);
     dx = x2 - x1;
     dy = y2 - y1;
@@ -20,7 +22,8 @@ int main() {
         step = abs(dy);
     xn = dx / step;
     yn = dy / step;
-    for (i = 1; i <= step; i++) {
+    for (i = 1; i <= step; i++)
+    {
         putpixel(x1, y1, LIGHTBLUE);
         delay(100);
         x1 += xn;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Copy [`MinGW`](https://sourceforge.net/projects/mingw/) folder to `C:\MinGW`
+- ## Put your code in `Home/src`
+- ## Use `Ctrl+Shift+B` to compile code.
+    - ### Output will be put in `Home/build`
+    - ### Open the exe files in `Home/build` to run the program
