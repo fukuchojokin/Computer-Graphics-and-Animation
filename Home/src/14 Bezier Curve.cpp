6feac3e8e6a39e3b65c7050e53f0bb4639e98276
bestware <kshitiz.wagle@yahoo.com>
2023-01-01 02:03:58 +0545

    done

--- Home/src/14 Bezier Curve.cpp
+++ Home/src/14 Bezier Curve.cpp
@@ -1,21 +1,18 @@
-// Program for Bezier curve:
+#include <conio.h>
 #include <graphics.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 void bezier(int x[4], int y[4]) {
     int gd = DETECT, gm;
-    int i;
-    double t;
     initgraph(&gd, &gm, NULL);
-    for (t = 0.0; t < 1.0; t += 0.0005) {
+    for (double t = 0.0; t < 1.0; t += 0.0005) {
         double xt = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] +
                     3 * pow(t, 2) * (1 - t) * x[2] + pow(t, 3) * x[3];
         double yt = pow(1 - t, 3) * y[0] + 3 * t * pow(1 - t, 2) * y[1] +
                     3 * pow(t, 2) * (1 - t) * y[2] + pow(t, 3) * y[3];
         putpixel(xt, yt, WHITE);
     }
-    for (i = 0; i < 4; i++) putpixel(x[i], y[i], YELLOW);
+    for (int i = 0; i < 4; i++) putpixel(x[i], y[i], YELLOW);
     getch();
     closegraph();
 }
--- Home/src/9 Translation.cpp
+++ Home/src/9 Translation.cpp
@@ -6,8 +6,10 @@ void translate(int x1, int y1, int x2, int y2, int x, int y) {
     int graphdriver = DETECT, graphmode, errorcode;
     initgraph(&graphdriver, &graphmode, NULL);
     line(x1, y1, x2, y2);
-    x1 += x; y1 += y;
-    x2 += x; y2 += y;
+    x1 += x;
+    y1 += y;
+    x2 += x;
+    y2 += y;
     printf("Line after translation");
     line(x1, y1, x2, y2);
     getch();
