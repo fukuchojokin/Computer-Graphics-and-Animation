07f19544ec985d5c9c718be9437773597227c4eb
bestware <kshitiz.wagle@yahoo.com>
2023-01-01 12:13:34 +0545

    rotation

--- Home/src/10 2D Rotation.cpp
+++ Home/src/10 2D Rotation.cpp
@@ -2,22 +2,18 @@
 #include <graphics.h>
 #include <math.h>
 #include <stdio.h>
-void rotate2D(int x2, int y2, int x1, int y1, int th) {
-    double r11, r12, r21, r22;
+void rotate2D(int x1, int y1, int x2, int y2, int r) {
     line(x1, y1, x2, y2);
-    r11 = cos((th * 3.1428) / 180);
-    r12 = sin((th * 3.1428) / 180);
-    r21 = (-sin((th * 3.1428) / 180));
-    r22 = cos((th * 3.1428) / 180);
-    int xn = ((x2 * r11) - (y2 * r12));
-    int yn = ((x2 * r12) + (y2 * r11));
-    line(x1, y1, xn, yn);
+    float t = 3.14 * r / 180;
+    x2 = abs(x2 * cos(t) - y2 * sin(t));
+    y2 = abs(x2 * sin(t) + y2 * cos(t));
+    line(x1, y1, x2, y2);
     getch();
 }
 
 int main() {
     int graphdriver = DETECT, graphmode, errorcode;
     initgraph(&graphdriver, &graphmode, NULL);
-    rotate2D(200, 200, 200, 300, 15);
+    rotate2D(200, 200, 400, 200, 90);
     closegraph();
 }
